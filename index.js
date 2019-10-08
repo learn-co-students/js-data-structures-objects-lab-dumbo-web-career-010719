506c2a87d3ab826c5cfcd8b89caad88e0f25a1cb
sbrij001 <sbrij001@yahoo.com>
2019-10-08 13:46:54 -0400

    created psuedocode for updateDriverWithKeyAndValue funct

--- index.js
+++ index.js
@@ -1,2 +1,8 @@
 // Write your solution in this file!
 const driver = {};
+
+// updateDriverWithKeyAndValue(driver, key, value)
+//   1) returns a driver with the original key value pairs and the new key value pair
+//   2) it does not modify the original driver, but rather returns a clone with the new data
+// destructivelyUpdateDriverWithKeyAndValue(driver, key, value)
+//   3) updates `driver` with the given `key` and `value` (it is destructive) and returns the entire updated driver
