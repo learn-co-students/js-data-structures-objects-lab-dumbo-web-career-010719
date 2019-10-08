fca5713b0c352361e3badce3375f1e85a8b851a5
sbrij001 <sbrij001@yahoo.com>
2019-10-08 15:31:38 -0400

    created psuedo code and deleteFromDriverByKey(driver, key) function

--- index.js
+++ index.js
@@ -1,6 +1,4 @@
-// Write your solution in this file!
 const driver = {};
-
 // updateDriverWithKeyAndValue(driver, key, value)
 //   1) returns a driver with the original key value pairs and the new key value pair
 //   2) it does not modify the original driver, but rather returns a clone with the new data
@@ -11,12 +9,17 @@ function updateDriverWithKeyAndValue(driver,key,value){
   const newObj = {...driver}
   newObj[key] = value;
   return newObj;
-}
+};
 
 // destructivelyUpdateDriverWithKeyAndValue(driver, key, value)
 // 1) updates `driver` with the given `key` and `value` (it is destructive)
 // returns the entire updated driver
-
 function destructivelyUpdateDriverWithKeyAndValue(driver, key, value){
-  
-}
+  return Object.assign(driver, {[key]: value})
+};
+
+// deleteFromDriverByKey(driver, key)
+// deletes `key` from a clone of driver and returns the new driver (it is non-destructive):
+function deleteFromDriverByKey(driver, key){
+
+};
